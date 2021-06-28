42e27af6b35dd61ea25ac6e0018feef5ed82d5e6
CloudHolic <bluegkstjr@kaist.ac.kr>
2021-06-28 16:19:08 +0900

    Initial commit

---
+++ CloudSynth/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,19 @@
+﻿using Prism.Mvvm;
+
+namespace CloudSynth.ViewModels
+{
+    public class MainWindowViewModel : BindableBase
+    {
+        private string _title = "CloudSynth";
+        public string Title
+        {
+            get => _title;
+            set => SetProperty(ref _title, value);
+        }
+
+        public MainWindowViewModel()
+        {
+
+        }
+    }
+}
