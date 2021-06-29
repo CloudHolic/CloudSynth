4d13002f47731472384c7ab70584e23e989c986d
CloudHolic <bluegkstjr@kaist.ac.kr>
2021-06-30 08:55:57 +0900

    Add command

--- Modules/CloudSynth.Piano/Controls/PianoControl.xaml.cs
+++ Modules/CloudSynth.Piano/Controls/PianoControl.xaml.cs
@@ -1,17 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace CloudSynth.Piano.Controls
 {
@@ -20,9 +10,60 @@ namespace CloudSynth.Piano.Controls
     /// </summary>
     public partial class PianoControl : UserControl
     {
+        private bool _canExecute;
+        private EventHandler _canExecuteChanged;
+
+        public static readonly DependencyProperty KeyboardCommandProperty =
+            DependencyProperty.Register("KeyboardCommand", typeof(ICommand), typeof(PianoControl),
+                new PropertyMetadata(OnKeyboardCommandChanged));
+
         public PianoControl()
         {
             InitializeComponent();
+        }
+
+        public ICommand KeyboardCommand
+        {
+            get => (ICommand) GetValue(KeyboardCommandProperty);
+            set => SetValue(KeyboardCommandProperty, value);
+        }
+
+        protected override bool IsEnabledCore => base.IsEnabledCore && _canExecute;
+
+        public static void OnKeyboardCommandChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var p = (PianoControl)d;
+            p.HookUpCommand((ICommand) e.OldValue, (ICommand) e.NewValue);
+        }
+
+        private void AddCommand(ICommand command)
+        {
+            var handler = new EventHandler(CanExecuteChanged);
+            _canExecuteChanged = handler;
+            if (command != null)
+                command.CanExecuteChanged += _canExecuteChanged;
+        }
+
+        private void CanExecuteChanged(object sender, EventArgs e)
+        {
+            if (KeyboardCommand != null)
+                _canExecute = KeyboardCommand.CanExecute(null);
+
+            CoerceValue(IsEnabledProperty);
+        }
+        
+        private void HookUpCommand(ICommand oldCommand, ICommand newCommand)
+        {
+            if (oldCommand != null)
+                RemoveCommand(oldCommand);
+
+            AddCommand(newCommand);
+        }
+        
+        private void RemoveCommand(ICommand command)
+        {
+            EventHandler handler = CanExecuteChanged;
+            command.CanExecuteChanged -= handler;
         }
     }
 }
