68f694e1330d1e93db2f6587370130def45747c7
CloudHolic <bluegkstjr@kaist.ac.kr>
2021-06-29 09:27:26 +0900

    Implement window title bar

--- CloudSynth/Views/MainWindow.xaml.cs
+++ CloudSynth/Views/MainWindow.xaml.cs
@@ -1,11 +1,11 @@
-﻿using System.Windows;
+﻿using CloudSynth.Controls;
 
 namespace CloudSynth.Views
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : CustomWindow
     {
         public MainWindow()
         {
