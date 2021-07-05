96e7e674a82f3d06fddeb0faed93e3982e9a21c6
CloudHolic <bluegkstjr@kaist.ac.kr>
2021-07-05 15:25:51 +0900

    Add graph library

--- Modules/CloudSynth.Graph/ViewModels/GraphViewModel.cs
+++ Modules/CloudSynth.Graph/ViewModels/GraphViewModel.cs
@@ -1,20 +1,21 @@
 ﻿using CloudSynth.Core.Interfaces;
+using LiveCharts;
 using Prism.Regions;
 
 namespace CloudSynth.Graph.ViewModels
 {
     public class GraphViewModel : ViewModelBase
     {
-        private string _message;
-        public string Message
+        private ChartValues<double> _values1;
+        public ChartValues<double> Values1
         {
-            get => _message;
-            set => SetProperty(ref _message, value);
+            get => _values1;
+            set => SetProperty(ref _values1, value);
         }
 
         public GraphViewModel(IRegionManager regionManager) : base(regionManager)
         {
-            Message = "Graph View";
+            Values1 = new ChartValues<double> {1, 2, 3, 4, 5};
         }
     }
 }
